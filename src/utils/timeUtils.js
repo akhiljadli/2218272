754fdf0bc269a98df893c8f3c8aa987aa90f8e78
Akhil Jadli <125429520+akhiljadli@users.noreply.github.com>
2025-07-15 13:01:37 +0530

    Delete src directory

--- src/utils/timeUtils.js
+++
@@ -1,7 +0,0 @@
-export const addMinutes = (date, minutes) => {
-  return new Date(date.getTime() + minutes * 60000);
-};
-
-export const isAfter = (date1, date2) => {
-  return date1 > date2;
-};
--- src/utils/validateCustomCode.js
+++
@@ -1,3 +0,0 @@
-const validateCustomCode = (code) => /^[a-zA-Z0-9]{4,12}$/.test(code);
-
-export default validateCustomCode;
